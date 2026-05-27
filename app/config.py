92fe40edb3fcc664c0092d40d0dc81073a6ba94b
MadeINevi1 <danielborisov56@gmail.com>
2026-05-27 04:24:17 +0300

    Add SQLite deploy version

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-import os
+﻿import os
 from dotenv import load_dotenv
 
 
@@ -7,9 +7,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-key"
-    DATABASE_URL = os.environ.get("DATABASE_URL") or "postgresql://neondb_owner:npg_T3veMjhxHOR7@ep-small-wind-aqos5hiu-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-    DB_SERVER = os.environ.get("DB_SERVER") or "localhost"
-    DB_PORT = os.environ.get("DB_PORT") or "5432"
-    DB_NAME = os.environ.get("DB_NAME") or "fitness_app"
-    DB_USER = os.environ.get("DB_USER") or "postgres"
-    DB_PASSWORD = os.environ.get("DB_PASSWORD") or "1234"
+    DB_PATH = os.environ.get("DB_PATH") or os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        "fitness_app.db"
+    )
